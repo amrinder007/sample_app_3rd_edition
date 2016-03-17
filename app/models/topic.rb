4354a7ca12ccdbab5060e85ca33691a500b6e152
Amrinder Singh <amrinder.king@yahoo.com>
2016-03-17 13:02:23 +0530

    little style added to topics

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,5 +2,5 @@ class Topic < ActiveRecord::Base
 	has_many :pt_relationships
 	has_many :microposts, through: :pt_relationships
 
-	validates :name, presence: true, length: { maximum: 255 }, format: { with: /\A[a-zA-Z0-9]+\z/ }
+	validates :name, presence: true, length: { maximum: 255 }, format: { with: /\A[a-zA-Z0-9 ]+\z/ }
 end
