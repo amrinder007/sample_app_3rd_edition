c90d1c0c87048f3f3a18f61a94278afc2a106a59
Amrinder Singh <amrinder.king@yahoo.com>
2016-03-17 12:48:38 +0530

    validation added to topics

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -8,11 +8,26 @@ class Micropost < ActiveRecord::Base
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
   validate  :picture_size
+  validate  :topics_provided
 
-  after_save :save_topics
+  after_save :add_topics_to_post
   
   private
-  
+
+    # Validates the topics provided. Validation is definded in topics model.
+    def topics_provided
+      is_valid = true
+      topics_arr = get_topics_arr
+      topics_arr.each{|topic_name|
+        topic = Topic.where(name: topic_name).first_or_initialize
+        unless topic.save
+          is_valid = false
+          break
+        end
+      }
+      errors.add(:topic, "should not contain special characters and should be less than 255 characters") unless is_valid
+    end
+
     # Validates the size of an uploaded picture.
     def picture_size
       if picture.size > 5.megabytes
@@ -21,13 +36,17 @@ class Micropost < ActiveRecord::Base
     end
 
     # Saves topics for a post.
-    def save_topics
-      topics_arr = topic_list.split(",").reject{|t| t.to_s.empty?}
+    def add_topics_to_post
+      topics_arr = get_topics_arr
       topics_arr.each{|topic_name|
-        topic = Topic.where(name: topic_name).first_or_initialize
-        topic.save
-        
+        # topic = Topic.where(name: topic_name).first_or_initialize
+        # topic.save
+        topic = Topic.find_by(name: topic_name)
         pt_relationship = PtRelationship.create(micropost_id: id, topic_id: topic.id)
       }
+    end
+
+    def get_topics_arr
+      topic_list.split(",").reject{|t| t.to_s.empty?}.map{|t| t.strip}
     end
 end
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,4 +1,6 @@
 class Topic < ActiveRecord::Base
 	has_many :pt_relationships
 	has_many :microposts, through: :pt_relationships
+
+	validates :name, presence: true, length: { maximum: 255 }, format: { with: /\A[a-zA-Z0-9]+\z/ }
 end
